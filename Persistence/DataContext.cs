4aca6434b784a92c2700d52685673a7f4c1bf6dd
Shafqat Farhan <shafqat.f@datum.pk>
2019-10-23 12:37:44 -0700

    Initial Commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,38 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Value>()
+                .HasData(
+                    new Value
+                    {
+                        Id = 1,
+                        Name = "Value 101"
+                    },
+                    new Value
+                    {
+                        Id = 2,
+                        Name = "Value 102"
+                    },
+                    new Value
+                    {
+                        Id = 3,
+                        Name = "Value 103"
+                    }
+                );
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ Udemy-Reactivities/Controllers/WeatherForecastController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, DataContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Value>> Get(int Id)
+        {
+            var value = await _context.Values.FindAsync(Id);
+            return Ok(value);
+        }
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+    }
+}
